aafaecc24b1d1f3ce5e0bfa45cc7ac2232ed9198
shimeng.eric <shimeng.eric@bytedance.com>
2021-04-11 17:15:54 +0800

    fix: bugs

--- src/server.js
+++ src/server.js
@@ -9,30 +9,28 @@ app.use(cors({
 const server = require('http').createServer(app.callback());
 
 
-const io = require('socket.io')(server, {
-  path: '/api/webrtc/'
-}); 
+const io = require('socket.io')(server); 
 io.on('connection', socket => {
   socket.on('join', room => {
     console.log('join..', room);
     socket.join(room);
-    const myRoom = io.sockets.adapter.rooms[room];
+    const myRoom = io.sockets.adapter.rooms.get(room);
 
-    const users = Object.keys(myRoom.sockets).length;
+    const users = myRoom.size;
     // 给自己发消息
-    // socket.emit('joined', room, socket.id);
+    socket.emit('joined', room, socket.id);
     // 给房间的除了自己以外的所有人发消息
     // socket.to(room).emit('joined', room, socket.id);
     // 给房间的所有人回消息
     // io.in(room).emit('joined', room, socket.id);
     // 除自己，整个站点所有人发消息
-    socket.broadcast.emit('joined', room, socket.id);
+    // socket.broadcast.emit('joined', room, socket.id);
   });
   socket.on('leave', room => {
     socket.join(room);
-    const myRoom = io.sockets.adapter.rooms[room];
+    const myRoom = io.sockets.adapter.rooms.get(room);
 
-    const users = Object.keys(myRoom.sockets).length;
+    const users = myRoom.size;
     // 给自己发消息
     // socket.emit('joined', room, socket.id);
     // 给房间的除了自己以外的所有人发消息
